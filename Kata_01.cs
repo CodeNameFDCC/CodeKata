72eaf542f5f240c875bd9c462da40fd633cba723
wjdxo2412 <wjdxo2412@gmail.com>
2024-10-27 18:39:40 +0900

    CodeKata_01

---
+++ Kata_01.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+public class Kata_01
+{
+    public int solution(int num1, int num2) => num1 - num2;
+
+    public void Print(int a, int b)
+    {
+        var temp = solution(a, b);
+        Console.WriteLine(temp);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Code 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Kata_01().Print(5, 4);
+
+        }
+    }
+}
+
+   
